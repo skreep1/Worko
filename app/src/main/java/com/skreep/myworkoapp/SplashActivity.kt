d5c2bcaeb3068766b5f1708abb8f9ba295ff3faa
skreep1 <skrypka.m.l@gmail.com>
2021-07-30 23:15:12 +0300

    add SplashActivity.kt

--- app/src/main/java/com/skreep/myworkoapp/SplashActivity.kt
+++ app/src/main/java/com/skreep/myworkoapp/SplashActivity.kt
@@ -1,11 +1,26 @@
 package com.skreep.myworkoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        setBackgroundSecond()
+    }
+
+    private fun setBackgroundSecond() {
+
+        Handler(Looper.myLooper() ?: return).postDelayed({
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }, 2000)
     }
 }
