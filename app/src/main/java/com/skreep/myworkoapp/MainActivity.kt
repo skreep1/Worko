89db3d369d9394ea8c94d7652c062fd6541f8545
skreep1 <skrypka.m.l@gmail.com>
2021-07-30 21:32:31 +0300

    commit

--- app/src/main/java/com/skreep/myworkoapp/MainActivity.kt
+++ app/src/main/java/com/skreep/myworkoapp/MainActivity.kt
@@ -2,10 +2,64 @@ package com.skreep.myworkoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.*
+import com.skreep.myworkoapp.adapter.WorkoutAdapter
+import com.skreep.myworkoapp.model.WorkoutData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var dbref : DatabaseReference
+    private lateinit var userRecyclerview : RecyclerView
+    private lateinit var userArrayList : ArrayList<WorkoutData>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userRecyclerview = findViewById(R.id.recyclerworkoutList)
+        userRecyclerview.layoutManager = LinearLayoutManager(this)
+        userRecyclerview.setHasFixedSize(true)
+
+        userArrayList = arrayListOf<WorkoutData>()
+        getUserData()
+
+    }
+
+    private fun getUserData() {
+
+        dbref = FirebaseDatabase.getInstance().getReference("Workout")
+
+        dbref.addValueEventListener(object : ValueEventListener{
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()){
+
+                    for (userSnapshot in snapshot.children){
+
+
+                        val user = userSnapshot.getValue(WorkoutData::class.java)
+                        userArrayList.add(user!!)
+
+                    }
+
+                    userRecyclerview.adapter = WorkoutAdapter(userArrayList)
+
+
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
+
     }
 }
---
+++ app/src/main/java/com/skreep/myworkoapp/model/WorkoutData.kt
@@ -0,0 +1,7 @@
+package com.skreep.myworkoapp.model
+
+class WorkoutData(var name: String? = null,
+                  var workoutName: String? = null,
+                  var workoutDesc: String? = null,
+                  var workoutTime: String? = null) {
+}
